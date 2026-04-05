f2a94c7e38ae78bae1fcf2085ef53718d975ccae
MeledakCik <kakangdanadam@gmail.com>
2026-04-05 16:30:08 +0700

    terbaru

--- api/index.js
+++ api/index.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
